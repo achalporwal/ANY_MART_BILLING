3d8a0c3405f02c9e8ffd1595ab45500b44072b97
ACHAL PORWAL <39208532+achalporwal@users.noreply.github.com>
2026-06-25 17:59:05 +0530

    Update BillItem.java

--- src/models/BillItem.java
+++ src/models/BillItem.java
@@ -3,132 +3,131 @@ package models;
 import java.math.BigDecimal;
 
 public class BillItem {
-    private String billItemId;
-    private String billId;
-    private String productId;
-    private int quantity;
-    private BigDecimal mrp;
-    private BigDecimal prp;
-    private BigDecimal taxableValue;
-    private BigDecimal cgst;
-    private BigDecimal sgst;
-    private BigDecimal discount;
-    private BigDecimal finalAmount;
-    
-    // Product name loaded at runtime via JOIN
-    private String productName;
+private String billItemId;
+private String billId;
+private String productId;
+private int quantity;
+private BigDecimal mrp;
+private BigDecimal prp;
+private BigDecimal taxableValue;
+private BigDecimal cgst;
+private BigDecimal sgst;
+private BigDecimal discount;
+private BigDecimal finalAmount;
 
-    public BillItem() {}
+private String productName;
 
-    public BillItem(String billItemId, String billId, String productId, int quantity, 
-                    BigDecimal mrp, BigDecimal prp, BigDecimal taxableValue, 
-                    BigDecimal cgst, BigDecimal sgst, BigDecimal discount, BigDecimal finalAmount) {
-        this.billItemId = billItemId;
-        this.billId = billId;
-        this.productId = productId;
-        this.quantity = quantity;
-        this.mrp = mrp;
-        this.prp = prp;
-        this.taxableValue = taxableValue;
-        this.cgst = cgst;
-        this.sgst = sgst;
-        this.discount = discount;
-        this.finalAmount = finalAmount;
-    }
+public BillItem() {}
 
-    public String getBillItemId() {
-        return billItemId;
-    }
+public BillItem(String billItemId, String billId, String productId, int quantity, 
+BigDecimal mrp, BigDecimal prp, BigDecimal taxableValue, 
+BigDecimal cgst, BigDecimal sgst, BigDecimal discount, BigDecimal finalAmount) {
+this.billItemId = billItemId;
+this.billId = billId;
+this.productId = productId;
+this.quantity = quantity;
+this.mrp = mrp;
+this.prp = prp;
+this.taxableValue = taxableValue;
+this.cgst = cgst;
+this.sgst = sgst;
+this.discount = discount;
+this.finalAmount = finalAmount;
+}
 
-    public void setBillItemId(String billItemId) {
-        this.billItemId = billItemId;
-    }
+public String getBillItemId() {
+return billItemId;
+}
 
-    public String getBillId() {
-        return billId;
-    }
+public void setBillItemId(String billItemId) {
+this.billItemId = billItemId;
+}
 
-    public void setBillId(String billId) {
-        this.billId = billId;
-    }
+public String getBillId() {
+return billId;
+}
 
-    public String getProductId() {
-        return productId;
-    }
+public void setBillId(String billId) {
+this.billId = billId;
+}
 
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
+public String getProductId() {
+return productId;
+}
 
-    public int getQuantity() {
-        return quantity;
-    }
+public void setProductId(String productId) {
+this.productId = productId;
+}
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
+public int getQuantity() {
+return quantity;
+}
 
-    public BigDecimal getMrp() {
-        return mrp;
-    }
+public void setQuantity(int quantity) {
+this.quantity = quantity;
+}
 
-    public void setMrp(BigDecimal mrp) {
-        this.mrp = mrp;
-    }
+public BigDecimal getMrp() {
+return mrp;
+}
 
-    public BigDecimal getPrp() {
-        return prp;
-    }
+public void setMrp(BigDecimal mrp) {
+this.mrp = mrp;
+}
 
-    public void setPrp(BigDecimal prp) {
-        this.prp = prp;
-    }
+public BigDecimal getPrp() {
+return prp;
+}
 
-    public BigDecimal getTaxableValue() {
-        return taxableValue;
-    }
+public void setPrp(BigDecimal prp) {
+this.prp = prp;
+}
 
-    public void setTaxableValue(BigDecimal taxableValue) {
-        this.taxableValue = taxableValue;
-    }
+public BigDecimal getTaxableValue() {
+return taxableValue;
+}
 
-    public BigDecimal getCgst() {
-        return cgst;
-    }
+public void setTaxableValue(BigDecimal taxableValue) {
+this.taxableValue = taxableValue;
+}
 
-    public void setCgst(BigDecimal cgst) {
-        this.cgst = cgst;
-    }
+public BigDecimal getCgst() {
+return cgst;
+}
 
-    public BigDecimal getSgst() {
-        return sgst;
-    }
+public void setCgst(BigDecimal cgst) {
+this.cgst = cgst;
+}
 
-    public void setSgst(BigDecimal sgst) {
-        this.sgst = sgst;
-    }
+public BigDecimal getSgst() {
+return sgst;
+}
 
-    public BigDecimal getDiscount() {
-        return discount;
-    }
+public void setSgst(BigDecimal sgst) {
+this.sgst = sgst;
+}
 
-    public void setDiscount(BigDecimal discount) {
-        this.discount = discount;
-    }
+public BigDecimal getDiscount() {
+return discount;
+}
 
-    public BigDecimal getFinalAmount() {
-        return finalAmount;
-    }
+public void setDiscount(BigDecimal discount) {
+this.discount = discount;
+}
 
-    public void setFinalAmount(BigDecimal finalAmount) {
-        this.finalAmount = finalAmount;
-    }
+public BigDecimal getFinalAmount() {
+return finalAmount;
+}
 
-    public String getProductName() {
-        return productName;
-    }
+public void setFinalAmount(BigDecimal finalAmount) {
+this.finalAmount = finalAmount;
+}
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
+public String getProductName() {
+return productName;
+}
+
+public void setProductName(String productName) {
+this.productName = productName;
+}
 }
